e147996c7eae4fc8965751c82732a78084dbe689
nhyne <nhyne@nhyne.dev>
2020-04-20 23:15:15 -0400

    have zio running, however bazel builds everything

--- example/main.scala
+++ example/main.scala
@@ -1,5 +1,11 @@
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+import zio.{App, URIO, ZIO, console}
+import zio.console.Console
+
+object HelloWorld extends App {
+  val program : ZIO[Console, Nothing, Unit] = for {
+        _ <- console.putStrLn("Hello World!")
+    } yield {}
+
+    def run(args: List[String]): URIO[Console, Int] =
+    program.as(0)
 }
