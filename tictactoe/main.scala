dcac7979d9bd5cc9d625da8f6bc58e6707e8d62f
nhyne <nhyne@nhyne.dev>
2020-04-21 22:32:12 -0400

    zenv instead of console

--- tictactoe/main.scala
+++ tictactoe/main.scala
@@ -4,9 +4,9 @@ import zio.{App, ZEnv, ZIO, console}
 import zio.console.Console
 
 object TicTacToe extends App {
-  val program : ZIO[Console, Int, Unit] = console.putStrLn("Hello World!")
+    val program : ZIO[Console, Int, Unit] = console.putStrLn("Hello World!")
 
-    def run(args: List[String]): ZIO[Console, Nothing, Int] =
+    def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
         program.foldM(
             error => console.putStrLn(s"Execution failed with: $error") *> ZIO.succeed(1),
             _ => ZIO.succeed(0)
