213f7e19348ff39e7ff4c5e1f10c7826d4c3912c
nhyne <nhyne@nhyne.dev>
2020-04-21 22:28:13 -0400

    running with a failure case kind of?

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 pin:
 	bazel run @unpinned_scala_2_12//:pin
+
+explain:
+	bazel build --explain=explain.txt --verbose_explanations
--- tictactoe/main.scala
+++ tictactoe/main.scala
@@ -1,11 +1,14 @@
-import zio.{App, URIO, ZIO, console}
+package dev.nhyne.tictactoe
+
+import zio.{App, ZEnv, ZIO, console}
 import zio.console.Console
 
 object TicTacToe extends App {
-  val program : ZIO[Console, Nothing, Unit] = for {
-        _ <- console.putStrLn("Hello World!")
-    } yield {}
+  val program : ZIO[Console, Int, Unit] = console.putStrLn("Hello World!")
 
-    def run(args: List[String]): URIO[Console, Int] =
-    program.as(0)
+    def run(args: List[String]): ZIO[Console, Nothing, Int] =
+        program.foldM(
+            error => console.putStrLn(s"Execution failed with: $error") *> ZIO.succeed(1),
+            _ => ZIO.succeed(0)
+        )
 }
