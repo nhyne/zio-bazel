759caa17a785c64e9972c1ade7648d4959589d7d
nhyne <nhyne@nhyne.dev>
2020-04-23 17:40:14 -0400

    tests is passing!!!!

--- tictactoe/MenuCommandParserLive.scala
+++ tictactoe/MenuCommandParserLive.scala
@@ -4,7 +4,13 @@ import dev.nhyne.tictactoe.domain.MenuCommand
 import zio.UIO
 
 trait MenuCommandParserLive extends MenuCommandParser {
-    val menuCommandParser = new MenuCommandParser.Service[Any] {
-        def parse(input: String): UIO[MenuCommand] = ???
-    }
+  val menuCommandParser = new MenuCommandParser.Service[Any] {
+    def parse(input: String): UIO[MenuCommand] =
+      UIO.succeed(input).map {
+        case "new game" => MenuCommand.NewGame
+        case "resume"   => MenuCommand.Resume
+        case "quit"     => MenuCommand.Quit
+        case _          => MenuCommand.Invalid
+      }
+  }
 }
