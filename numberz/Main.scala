7ee1cd10ce410e5ba9d255a49fd487c2eb55c336
nhyne <nhyne@nhyne.dev>
2020-04-25 21:42:48 -0400

    got something compiling

--- numberz/Main.scala
+++ numberz/Main.scala
@@ -1,19 +1,17 @@
 package dev.nhyne.numberz
 
-import zio.console.Console
-import zio.{App, UIO, ZIO, console}
+import java.io.IOException
 
-object TicTacToe extends App {
-  val program: ZIO[Console, Int, Unit] = console.putStrLn("Hello World!")
+import zio.console.{Console, getStrLn, putStrLn}
+import zio.{App, ZEnv, ZIO, IO}
+import zio._
 
-  def run(args: List[String]): ZIO[Console, Nothing, Int] =
-    for {
-      out <- program
-        .foldM(
-          error => console.putStrLn("failure") *> UIO.succeed(1),
-          _ => UIO.succeed(0)
-        )
-    } yield {
-      out
-    }
+object Numberz extends App {
+  case class State(number: Int, currentGuess: Int) {
+    final def guess: Boolean = number == currentGuess
+  }
+  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = ???
+
+  val getGuess
+    : ZIO[Console, IOException, String] = putStrLn("something") *> getStrLn
 }
