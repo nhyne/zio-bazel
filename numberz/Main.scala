1feb258ff82ecdffe66bbb50d14786817c7f3db1
nhyne <nhyne@nhyne.dev>
2020-04-26 13:59:57 -0400

    completed guessing game

--- numberz/Main.scala
+++ numberz/Main.scala
@@ -2,16 +2,45 @@ package dev.nhyne.numberz
 
 import java.io.IOException
 
+import zio.random.{nextInt, Random}
 import zio.console.{Console, getStrLn, putStrLn}
 import zio.{App, ZEnv, ZIO, IO}
 import zio._
 
 object Numberz extends App {
-  case class State(number: Int, currentGuess: Int) {
-    final def guess: Boolean = number == currentGuess
+  case class State(number: Int, currentGuess: Int, guessNumber: Int) {
+    final def correct: Boolean = number == currentGuess
   }
-  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = ???
+  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
+        game.foldM(
+            _ => UIO.succeed(1),
+                _ => UIO.succeed(0)
+        )
+  }
 
   val getGuess
-    : ZIO[Console, IOException, String] = putStrLn("something") *> getStrLn
+    : ZIO[Console, IOException, Int] = for {
+      _ <- putStrLn("What's your guess?")
+      numString <- getStrLn
+  } yield {
+      numString.toInt
+  }
+
+    val pickNumber = nextInt(100)
+
+    val game = for {
+        number <- pickNumber
+        state = State(number = number, currentGuess = 0, guessNumber = 0)
+        _ <- gameLoop(state)
+    } yield ()
+
+    def gameLoop(state: State): ZIO[Console, IOException, State] = for {
+        _ <- putStrLn(s"${state.number}")
+        guess <- getGuess
+        newState = State(state.number, guess, state.guessNumber + 1)
+        _ <- if (newState.correct) putStrLn("You guessed correctly!!")
+        else putStrLn("Wrong, guess again")
+        state <- if(newState.correct) IO.succeed(newState) else gameLoop(newState)
+
+    } yield state
 }
