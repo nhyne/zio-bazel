ad904b69d30f8788ed2851a683962b154d9f9cbc
nhyne <nhyne@nhyne.dev>
2020-04-27 20:06:38 -0400

    have a task creator mode that returns menu mode

--- todo/mode/TaskCreatorMode.scala
+++ todo/mode/TaskCreatorMode.scala
@@ -1,28 +1,30 @@
 package dev.nhyne.todo.mode
 
-import dev.nhyne.todo.domain.Task
-import zio.{Has, ZIO, ZLayer, UIO}
-import zio.console.{Console, putStrLn, getStrLn}
+import dev.nhyne.todo.domain.{State, Task}
+import zio.{Has, ZIO, ZLayer}
+import zio.console.{Console, getStrLn, putStrLn}
 import java.io.IOException
 
 object TaskCreatorMode {
     trait Service {
-        def createTask(): ZIO[TaskCreatorMode, IOException, Task]
+        def createTask(state: State): ZIO[TaskCreatorMode, IOException, State]
     }
 
     type TaskCreatorMode = Has[Service] with Console
 
     val live = ZLayer.succeed(new Service {
-        def createTask(): ZIO[TaskCreatorMode, IOException, Task] = for {
+        def createTask(state: State): ZIO[TaskCreatorMode, IOException, State] = for {
              _ <- putStrLn("What do you need to get done?")
         taskTitle <- getStrLn
         _ <- putStrLn("Enter a description.")
         taskDescription <- getStrLn
         task = Task(taskTitle, taskDescription)
-      } yield task
+            newList = State.Menu(state.getList().addTask(task))
+
+      } yield newList
 
     })
 
-    def createTask(): ZIO[TaskCreatorMode, IOException, Task] =
-        ZIO.accessM[TaskCreatorMode](_.get.createTask())
+    def createTask(state: State): ZIO[TaskCreatorMode, IOException, State] =
+        ZIO.accessM[TaskCreatorMode](_.get.createTask(state))
 }
