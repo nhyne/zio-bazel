39d5f562272e8df61ca78dd3100571e5ff33e3de
nhyne <nhyne@nhyne.dev>
2020-05-23 21:51:10 -0400

    have it compiling, serving is throwing an error

--- todo/Main.scala
+++ todo/Main.scala
@@ -1,43 +1,51 @@
 package dev.nhyne.todo
 
-import zio.console.{Console, putStrLn}
+import cats.data.Kleisli
+import zio.console.putStrLn
 import zio.{App, RIO, ZEnv, ZIO}
 import cats.effect.ExitCode
 import dev.nhyne.todo.configuration.Configuration
 import dev.nhyne.todo.configuration.Configuration.Configuration
 import dev.nhyne.todo.persistence.TodoItemPersistenceService
 import dev.nhyne.todo.persistence.TodoItemPersistenceService.TaskPersistence
-import zio.clock.Clock
+import org.http4s.{Response, Request}
 import org.http4s.server.Router
 import org.http4s.implicits._
 
-import scala.concurrent.ExecutionContext
+//import scala.concurrent.ExecutionContext
 //import org.http4s.server.middleware.CORS
 import caliban.Http4sAdapter
 import zio.interop.catz._
 import org.http4s.server.blaze.BlazeServerBuilder
 import zio.blocking.Blocking
-import zio.logging._
+//import zio.logging._
 
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+//import scala.concurrent._
+//import ExecutionContext.Implicits.global
 
 object Main extends App {
 
-  val defaultLogger = Logging.console(
-    format = (_, logEntry) => logEntry,
-    rootLoggerName = Some("default-logger")
-  )
-  val todoPersistence = (Configuration.live ++ Blocking.live ++ defaultLogger) >>> TodoItemPersistenceService.live
-  type ProgramEnv = Console
-    with Clock
-    with Configuration
-    with Logging
-    with Clock
+//  val defaultLogger = Logging.console(
+//    format = (_, logEntry) => logEntry,
+//    rootLoggerName = Some("default-logger")
+//  )
+  val todoPersistence = (Configuration.live ++ Blocking.live) >>> TodoItemPersistenceService.live
+  type ProgramEnv = Configuration
     with TaskPersistence
-    with Blocking
+//    with Logging
+    with ZEnv
 
-  type AppTask[A] = RIO[ZEnv with TaskPersistence, A]
+  val query =
+    """
+          |{
+          |  getTodo(id: 1) {
+          |    title
+          |    description
+          |  }
+          |}
+          |""".stripMargin
+
+  type AppTask[A] = RIO[ProgramEnv, A]
 
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
     val program: ZIO[ProgramEnv, Throwable, Unit] = for {
@@ -45,7 +53,11 @@ object Main extends App {
       config <- Configuration.load
       api = GraphqlService.api
       interpreter <- api.interpreter
-      httpApp = Router[AppTask](
+      result <- interpreter.execute(query)
+      _ <- putStrLn(result.data.toString)
+      httpApp: Kleisli[AppTask, Request[AppTask], Response[AppTask]] = Router[
+        AppTask
+      ](
         "/graphql" -> Http4sAdapter.makeHttpService(interpreter)
 //        "/todos" -> ApiService(s"${config.api.endpoint}/todos").route
       ).orNotFound
@@ -63,7 +75,7 @@ object Main extends App {
 
     program
       .provideSomeLayer[ZEnv](
-        defaultLogger ++ Configuration.live ++ todoPersistence
+        Configuration.live ++ todoPersistence
       )
       .tapError(err => putStrLn(s"Execution failed with: $err"))
       .fold(_ => 1, _ => 0)
