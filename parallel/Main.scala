479e48496df9c2296beec8098d112148b769a887
nhyne <nhyne@nhyne.dev>
2020-09-28 19:54:05 -0400

    exponential backoff

--- parallel/Main.scala
+++ parallel/Main.scala
@@ -1,28 +1,38 @@
 package dev.nhyne.parallel
 
+import java.io.IOException
+
 import zio.{App, ExitCode, Schedule, UIO, URIO, ZIO}
-import zio.console.putStrLn
-import zio.random.nextInt
+import zio.duration._
+import zio.console.{putStrLn}
+import zio.random.nextIntBounded
 
 object Main extends App {
   def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
+    val tenTimes = Schedule.recurs(10)
+    val schedule = Schedule.exponential(1.seconds) && tenTimes
+
     val randomSleep = for {
-      time <- nextInt
+      time <- nextIntBounded(1000)
       _ <- putStrLn(s"Sleeping for $time")
+
       _ = Thread.sleep(time)
+      _ <- ZIO.getOrFail(None)
     } yield ()
 
-    val tenTimes = Schedule.recurs(10)
-
-    val program: ZIO[zio.ZEnv, Nothing, ExitCode] =
+    val program: ZIO[zio.ZEnv, IOException, ExitCode] =
       randomSleep
-        .retry(tenTimes)
+        .retry(schedule)
         .foldM(
           _ => UIO.succeed(ExitCode.success),
           _ => UIO.succeed(ExitCode.failure)
         )
 
     program
+      .foldM(
+        _ => UIO.succeed(ExitCode.success),
+        _ => UIO.succeed(ExitCode.failure)
+      )
   }
 }
