f71b0c726867f3c7ed226ff30ba493dca1508d9f
nhyne <nhyne@nhyne.dev>
2020-10-01 07:40:34 -0400

    have tests running now

--- parallel/MainSpec.scala
+++ parallel/MainSpec.scala
@@ -1,9 +1,11 @@
 package dev.nhyne.parallel
 
-import zio._
 import zio.test._
-import zio.test.Assertion._
 
 object MainSpec extends DefaultRunnableSpec {
 
+  def spec =
+    suite("some test spec") {
+      test("some test")(assert("some value")(Assertion.isNonEmptyString))
+    }
 }
