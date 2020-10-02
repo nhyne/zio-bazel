07aa08f01a10c1cb1babf66eb948b2979f1b406f
nhyne <nhyne@nhyne.dev>
2020-10-02 08:57:58 -0400

    have simple timed message service

--- parallel/Main.scala
+++ parallel/Main.scala
@@ -1,22 +1,17 @@
 package dev.nhyne.parallel
 
-import java.io.IOException
-
-import dev.nhyne.parallel.TimedMessageService
 import dev.nhyne.parallel.TimedMessageService.TimedMessage
-import zio.{App, ExitCode, Schedule, UIO, URIO, ZIO, ZLayer, clock, console, random}
-import zio.duration._
-import zio.console.{Console, putStrLn}
-import zio.random.nextIntBounded
+import zio.{App, ExitCode, UIO, URIO, ZIO}
+import zio.console.Console
 
 object Main extends App {
-  type ProgramEnv = Console with TimedMessage
+  type ProgramEnv = Console
 
   def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
-    val tenTimes = Schedule.recurs(10)
-    val schedule = Schedule.exponential(1.seconds) && tenTimes
-
+//    val tenTimes = Schedule.recurs(10)
+//    val schedule = Schedule.exponential(1.seconds) && tenTimes
+    val timedMessage = TimedMessageService.live
 //    val randomSleep = for {
 //      time <- nextIntBounded(1000)
 //      _ <- putStrLn(s"Sleeping for $time")
@@ -25,11 +20,13 @@ object Main extends App {
 //      _ <- ZIO.getOrFail(None)
 //    } yield ()
 
-    val program: ZIO[zio.ZEnv, IOException, ExitCode] = for {
-
-    }
+    val program: ZIO[TimedMessage, Nothing, Unit] = for {
+      _ <-
+        TimedMessageService.message("cool", zio.duration.Duration.Finite(1000))
+    } yield ()
 
     program
+      .provideSomeLayer[zio.ZEnv](timedMessage)
       .foldM(
         _ => UIO.succeed(ExitCode.success),
         _ => UIO.succeed(ExitCode.failure)
--- parallel/TimedMessageService.scala
+++ parallel/TimedMessageService.scala
@@ -1,32 +1,27 @@
 package dev.nhyne.parallel
 
-import zio.console.{putStrLn, Console}
 import zio.{Has, ZIO, ZLayer}
 import zio.duration.Duration
-
-class TimedMessageService {}
 
 object TimedMessageService {
   type TimedMessage = Has[Service]
   trait Service {
-    def message(msg: String, delay: Duration): ZIO[Console, Nothing, Unit]
+    def message(msg: String, delay: Duration): ZIO[TimedMessage, Nothing, Unit]
   }
 
   val live: ZLayer[
-    Console,
+    zio.ZEnv,
     Throwable,
     TimedMessage
   ] = ZLayer.succeed(new Live {})
 
-  def message(msg: String, delay: Duration): ZIO[Console, Nothing, Unit] =
-    ZIO.accessM[TimedMessage](_.get.message(msg, delay))
+  def message(msg: String, delay: Duration): ZIO[TimedMessage, Nothing, Unit] =
+    ZIO.accessM(_.get.message(msg, delay))
 }
 
 trait Live extends TimedMessageService.Service {
   def message(msg: String, delay: Duration) = {
     Thread.sleep(delay.toMillis)
-    for {
-      _ <- putStrLn(msg)
-    } yield ()
+    ZIO.succeed(println(msg))
   }
 }
